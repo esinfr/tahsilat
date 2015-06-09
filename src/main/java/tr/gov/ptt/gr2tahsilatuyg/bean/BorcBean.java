e6c38dfdb2b332ca0a084d497254df3c9f2c63d9
Administrator <Administrator@10.10.10.134>
2015-06-09 10:58:20 +0300

    03.06.2015 menuler vt den çekildi.

--- src/main/java/tr/gov/ptt/gr2tahsilatuyg/bean/BorcBean.java
+++ src/main/java/tr/gov/ptt/gr2tahsilatuyg/bean/BorcBean.java
@@ -10,6 +10,7 @@ import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 import tr.gov.ptt.gr2tahsilatuyg.entity.Borc;
 import tr.gov.ptt.gr2tahsilatuyg.service.BorcService;
 
@@ -18,12 +19,22 @@ import tr.gov.ptt.gr2tahsilatuyg.service.BorcService;
  * @author Administrator
  */
 @ManagedBean
-@RequestScoped
+@SessionScoped
 public class BorcBean {
     
     private Borc borc;
     
     private List<Borc> borcListesi;
+    
+    private List<Borc> seciliBorclar;
+
+    public List<Borc> getBorcListesi() {
+        return borcListesi;
+    }
+
+    public void setBorcListesi(List<Borc> borcListesi) {
+        this.borcListesi = borcListesi;
+    }
     
     @EJB
     private BorcService borcService;
@@ -32,12 +43,22 @@ public class BorcBean {
     public BorcBean() {
         borc=new Borc();
         borcListesi=new ArrayList<Borc>();
+        seciliBorclar = new ArrayList<Borc>();
     }
 
 
     public Borc getBorc() {
         return borc;
     }
+
+    public List<Borc> getSeciliBorclar() {
+        return seciliBorclar;
+    }
+
+    public void setSeciliBorclar(List<Borc> seciliBorclar) {
+        this.seciliBorclar = seciliBorclar;
+    }
+    
 
     public void setBorc(Borc borc) {
         this.borc = borc;
@@ -46,10 +67,22 @@ public class BorcBean {
    public String borcListele()
    {
        borcListesi = borcService.borclariGetir(borc);
+       borc=borcListesi.get(0);
        return "tahsilatListele.xhtml?faces-redirect=true";
        
    }
      
+   public List<String> tamamlaKurumAdi(String p_sorgu)
+    {
+      
+        return borcService.tamamlaKurumAdi(p_sorgu);
+    }
+   
+      
+//    public int kurumIdGetir(String p_kurumAd){
+//       
+//     return borcService.kurumIdGetir(p_kurumAd);
+//    }
     
     
 }
